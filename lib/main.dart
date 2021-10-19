60ae651f7e295df903082861370f289855cea2d8
TTC <banglajoy701@gmail.com>
2021-10-19 03:20:33 -0700

    4th comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:akhtar/my_home_page.dart';
 import 'package:akhtar/second_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,90 +15,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
-    );
-  }
-}
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("akhtaruz zaman"),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                  onPressed:(){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondPage())) ;
-                  },
-                  child:Text("Go To Second page")
-              ),
-              RaisedButton(
-                  color: Colors.purple,
-                  child: Text(btnText1),
-                  onPressed: (){
-                    setState(() {
-                    btnText1="Button Pressed";
-                      imgVis1=true;
-                    });
-
-                  }),
-              Visibility(
-                visible: imgVis1,
-                child: Container(
-                    height: 500,
-                    width: 400,
-                    child: Image.network("https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg")
-                ),
-
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Container(
-                      height: 200,
-                        width: 250,
-                        child: Image.asset("assets/images/imge4.jpeg")
-                    ),
-                    Container(
-                        height: 200,
-                        width: 250,
-                        child: Image.asset("assets/images/imge4.jpeg")
-                    ),
-                    Container(
-                        height: 200,
-                        width: 250,
-                        child: Image.asset("assets/images/imge4.jpeg")
-                    ),
-                    Container(
-                        height: 200,
-                        width: 250,
-                        child: Image.asset("assets/images/imge4.jpeg")
-                    ),
-                    Container(
-                        height: 200,
-                        width: 250,
-                        child: Image.asset("assets/images/imge4.jpeg")
-                    ),
-
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-
     );
   }
 }
